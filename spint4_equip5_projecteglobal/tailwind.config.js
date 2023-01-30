c583941de4978b386ccc124843ef529523bb9e4c
Eduard35 <eduardandreizahorneanu@iesmontsia.org>
2023-01-30 16:57:37 +0100

    arreglar tailwind

--- spint4_equip5_projecteglobal/tailwind.config.js
+++ spint4_equip5_projecteglobal/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",],
   theme: {
     extend: {},
   },
