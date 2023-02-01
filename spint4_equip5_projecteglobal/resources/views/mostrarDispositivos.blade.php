8cb5fe49b9b6460af9806c1bfff35a6d43d8e50d
janalmudeve <joanpasqualalmudeve@iesmontsia.org>
2023-02-01 21:01:42 +0100

    taula dispositius tailwind

--- spint4_equip5_projecteglobal/resources/views/mostrarDispositivos.blade.php
+++ spint4_equip5_projecteglobal/resources/views/mostrarDispositivos.blade.php
@@ -3,7 +3,35 @@
 @extends('layouts.header')
 
 @section('content')
-
+<h1 class="text-center p-8 mt-2 text-3xl">Listado dispositivos</h1>
+<div style="display:flex; justify-content:center">
+<table class="container table-auto w-full rounded-lg border-2 border-orange-500 bg-white mt-5 w-99">
+    <thead class="bg-orange-500 text-white">
+      <tr>
+        <th>Song</th>
+        <th>Artist</th>
+        <th>Year</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="bg-orange-100">
+        <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+        <td>Malcolm Lockyer</td>
+        <td>1961</td>
+      </tr>
+      <tr class="bg-orange-100">
+        <td>Witchy Woman</td>
+        <td>The Eagles</td>
+        <td>1972</td>
+      </tr>
+      <tr class="bg-orange-100">
+        <td>Shining Star</td>
+        <td>Earth, Wind, and Fire</td>
+        <td>1975</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 @stop
 
 <html>
