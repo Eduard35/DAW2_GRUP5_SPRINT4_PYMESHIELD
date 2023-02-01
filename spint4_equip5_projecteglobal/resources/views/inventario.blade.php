bac6a83ed951fa84a25bc7db8b0f29605c14944e
Eduard35 <eduardandreizahorneanu@iesmontsia.org>
2023-02-01 20:52:09 +0100

    Update inventario.blade.php

--- spint4_equip5_projecteglobal/resources/views/inventario.blade.php
+++ spint4_equip5_projecteglobal/resources/views/inventario.blade.php
@@ -4,41 +4,6 @@
 
 @section('content')
 
-
-<table class="table-auto">
-    <thead>
-      <tr>
-        <th>Marca</th>
-        <th>Model</th>
-        <th>MAC</th>
-        <th>Wifi</th>
-        <th>Tipo</th>
-        <th>Descripción</th>
-        <th>Estado</th>
-        <th>Num Série</th>
-        <th><button>Crear</button></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-        <td>Malcolm Lockyer</td>
-        <td>1961</td>
-      </tr>
-      <tr>
-        <td>Witchy Woman</td>
-        <td>The Eagles</td>
-        <td>1972</td>
-      </tr>
-      <tr>
-        <td>Shining Star</td>
-        <td>Earth, Wind, and Fire</td>
-        <td>1975</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-
 @stop
 
 <html>
