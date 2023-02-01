ec646d77429c66cba03c4baad8086a4872f14f5f
Eduard35 <eduardandreizahorneanu@iesmontsia.org>
2023-02-01 21:23:00 +0100

    Update inventario.blade.php

--- spint4_equip5_projecteglobal/resources/views/inventario.blade.php
+++ spint4_equip5_projecteglobal/resources/views/inventario.blade.php
@@ -4,6 +4,62 @@
 
 @section('content')
 
+<h1 class="text-center p-8 mt-2 text-3xl">Inventario</h1>
+<div style="display:flex; justify-content:center">
+<table class="container table-auto w-full rounded-lg border-2 border-orange-500 bg-white mt-5 w-99">
+    <thead class="bg-orange-500 text-white">
+      <tr>
+        <th></th>
+        <th>Marca</th>
+        <th>Model</th>
+        <th>MAC</th>
+        <th>Wifi</th>
+        <th>Tipo</th>
+        <th>Descripcion</th>
+        <th>Estado</th>
+        <th>Núm Serie</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="bg-orange">
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr class="bg-orange">
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr class="bg-orange">
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr class="bg-orange">
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr class="bg-orange">
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr class="bg-orange">
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr class="bg-orange">
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
 @stop
 
 <html>
