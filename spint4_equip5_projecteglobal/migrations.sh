9ae1966aedd2e1fb40846591f46893e4b8053413
Eduard35 <eduardandreizahorneanu@iesmontsia.org>
2023-02-06 19:47:35 +0100

    proves amb aleix escri

--- spint4_equip5_projecteglobal/migrations.sh
+++ spint4_equip5_projecteglobal/migrations.sh
@@ -2,9 +2,9 @@
 wget -O pymedb.zip https://github.com/Tupiet-Classe/PymeDB/archive/refs/heads/main.zip
 unzip pymedb.zip -d pymedb
 folder='pymedb/PymeDB-main'
-mv $folder/factories/* database/factories 
-mv $folder/migrations/* database/migrations 
-mv $folder/seeders/* database/seeders 
-mv $folder/Models/* app/Models 
+mv $folder/factories/* database/factories
+mv $folder/migrations/* database/migrations
+mv $folder/seeders/* database/seeders
+mv $folder/Models/* app/Models
 rm -rf pymedb
 rm pymedb.zip
