1f4f25b928b726cca1069d4161cee55b736dedf1
Tupiet <aleixescrihuela@gmail.com>
2023-02-07 19:04:31 +0100

    Guardar imatges - v2

--- spint4_equip5_projecteglobal/app/Http/Controllers/ImageDeviceController.php
+++ spint4_equip5_projecteglobal/app/Http/Controllers/ImageDeviceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\ImageDevice;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 
 class ImageDeviceController extends Controller
@@ -12,6 +13,6 @@ class ImageDeviceController extends Controller
     }
 
     public function guardar(Request $request) {
-        
+        Storage::disk('local')->put('hola.txt', 'hola');
     }
 }
