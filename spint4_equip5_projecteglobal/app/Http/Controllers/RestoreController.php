dab64b3cf069ea225e9b6d38edda13116cb816f6
Eduard35 <eduardandreizahorneanu@iesmontsia.org>
2023-02-07 23:50:07 +0100

    arreglar funcio listar

--- spint4_equip5_projecteglobal/app/Http/Controllers/RestoreController.php
+++ spint4_equip5_projecteglobal/app/Http/Controllers/RestoreController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Device;
 use Illuminate\Http\Request;
 
 class RestoreController extends Controller
 {
+
+    //Funcio per a llistar
     public function listar(){
-        return view("restaurar");
+
+        $listar = Device::all()->where("hidden", "!=", null);
+        return view("restaurar", compact("listar"));
     }
 
     public function restaurar(){
