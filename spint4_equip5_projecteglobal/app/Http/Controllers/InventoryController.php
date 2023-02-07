b9dca337f85965c7fc31676e8f649239064c9535
Aleix <aleixpaga@iesmontsia.org>
2023-02-07 21:25:20 +0100

    Taula de Inventari mostra dades bbdd

--- spint4_equip5_projecteglobal/app/Http/Controllers/InventoryController.php
+++ spint4_equip5_projecteglobal/app/Http/Controllers/InventoryController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Device;
+
 
 class InventoryController extends Controller
 {
-    //
+    public function inventario(){
+        $dispositivosInventario = Device :: all();
+        // $dispositivosInventario = Device::whereIn('id', $dispositivosInventario->pluck('id'))->paginate(10);
+        return view("inventario", compact('dispositivosInventario'));
+    }
 }
--- spint4_equip5_projecteglobal/routes/web.php
+++ spint4_equip5_projecteglobal/routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\RestoreController;
+use App\Http\Controllers\InventoryController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +37,7 @@ Route::put('/dispositivo', [DevicesController::class, 'modificar']);
 Route::patch('/dispositivo', [DevicesController::class, 'eliminar']);
 
 //Mostrar inventari
-Route::get('/inventario', [MainController::class, 'inventario']);
+Route::get('/inventario', [InventoryController::class, 'inventario']);
 
 
 Route::get('/sendImage', [MainController::class, 'sendImage']);
