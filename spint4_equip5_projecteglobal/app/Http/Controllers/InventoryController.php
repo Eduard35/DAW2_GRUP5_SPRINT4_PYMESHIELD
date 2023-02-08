3fd5a84d3bff8d9fb64f677e32ed7fbea49573d7
janalmudeve <joanpasqualalmudeve@iesmontsia.org>
2023-02-08 19:00:15 +0100

    mostrarDispositiu millorat dades DDBB

--- spint4_equip5_projecteglobal/app/Http/Controllers/InventoryController.php
+++ spint4_equip5_projecteglobal/app/Http/Controllers/InventoryController.php
@@ -9,7 +9,7 @@ use App\Models\Device;
 class InventoryController extends Controller
 {
     public function inventario(){
-        $dispositivosInventario = Device :: all();
+        $dispositivosInventario = Device::all();
         $dispositivosInventario = Device::whereIn('type_device_id', $dispositivosInventario->pluck('type_device_id'))->paginate(10);
         return view("inventario", compact('dispositivosInventario'));
     }
