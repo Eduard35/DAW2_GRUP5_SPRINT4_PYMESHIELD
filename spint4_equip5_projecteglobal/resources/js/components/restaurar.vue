9377ca28773f5425df152339ba765c7be6061251
Eduard35 <eduardandreizahorneanu@iesmontsia.org>
2023-02-14 16:16:21 +0100

    phpmyadmin i part1 restaurar amb vue

--- spint4_equip5_projecteglobal/resources/js/components/restaurar.vue
+++ spint4_equip5_projecteglobal/resources/js/components/restaurar.vue
@@ -1,39 +1,44 @@
 <!-- Codi de la pàgina -->
 <template>
 
-    <table>
-        <tr>
-            <th>Marca</th>
-            <th>Modelo</th>
-            <th>MAC Ethernet</th>
-            <th>MAC WiFi</th>
-            <th>Tipo</th>
-            <th>Descripción</th>
-            <th>Estado</th>
-            <th>Etiqueta</th>
-            <th>Núm Serie</th>
-            <th>Usuario</th>
-            <th></th>
-        </tr>
-        <tr v-for="dispositivo in listar" :key="dispositivo.id">
-            <td>{{ dispositivo.brand }}</td>
-            <td>{{ dispositivo.model }}</td>
-            <td>{{ dispositivo.mac_ethernet }}</td>
-            <td>{{ dispositivo.mac_wifi }}</td>
-            <td>{{ dispositivo.type_device_id }}</td>
-            <td>{{ dispositivo.description }}</td>
-            <td>{{ dispositivo.state }}</td>
-            <td>{{ dispositivo.tag }}</td>
-            <td>{{ dispositivo.serial_number }}</td>
-            <td>{{ dispositivo.user_id }}</td>
-            <td>
-                <button
-                    class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                    Restaurar
-                </button>
-            </td>
-        </tr>
-    </table>
+    <div class="flex justify-center -m-1.5">
+        <table
+            class="container shadow-xl table-auto w-full rounded-lg border-2 border-orange-500 bg-white mt-5 w-99 text-center">
+            <tr>
+                <thead class="bg-orange-500 text-white">
+                    <th>Marca</th>
+                    <th>Modelo</th>
+                    <th>MAC Ethernet</th>
+                    <th>MAC WiFi</th>
+                    <th>Tipo</th>
+                    <th>Descripción</th>
+                    <th>Estado</th>
+                    <th>Etiqueta</th>
+                    <th>Núm Serie</th>
+                    <th>Usuario</th>
+                    <th></th>
+                </thead>
+            </tr>
+            <tr v-for="dispositivo in listar" :key="dispositivo.id" class="bg-orange-100 border-b hover:bg-orange-300">
+                <td>{{ dispositivo.brand }}</td>
+                <td>{{ dispositivo.model }}</td>
+                <td>{{ dispositivo.mac_ethernet }}</td>
+                <td>{{ dispositivo.mac_wifi }}</td>
+                <td>{{ dispositivo.type_device_id }}</td>
+                <td>{{ dispositivo.description }}</td>
+                <td>{{ dispositivo.state }}</td>
+                <td>{{ dispositivo.tag }}</td>
+                <td>{{ dispositivo.serial_number }}</td>
+                <td>{{ dispositivo.user_id }}</td>
+                <td>
+                    <button
+                        class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                        Restaurar
+                    </button>
+                </td>
+            </tr>
+        </table>
+    </div>
 
 </template>
 
@@ -48,10 +53,7 @@
 export default {
     data() {
         return {
-            listar: [
-                { id: 1, brand: 'Apple', model: 'iPhone X', mac_ethernet: '00:11:22:33:44:55', mac_wifi: 'AA:BB:CC:DD:EE:FF', type_device_id: 'Smartphone', description: 'iPhone X en excelentes condiciones', state: 'Activo', tag: 'A123', serial_number: '123456', user_id: 1 },
-                { id: 2, brand: 'Samsung', model: 'Galaxy S21', mac_ethernet: '11:22:33:44:55:66', mac_wifi: 'BB:CC:DD:EE:FF:AA', type_device_id: 'Smartphone', description: 'Galaxy S21 en buen estado', state: 'Inactivo', tag: 'B456', serial_number: '234567', user_id: 2 }
-            ]
+            listar
         }
     }
 }
