39db516c9791f6125a97e76c5f088af975fb7d5e
Aleix <aleixpaga@iesmontsia.org>
2023-02-14 20:27:01 +0100

    funcionalitat eliminar dipositiu

--- spint4_equip5_projecteglobal/app/Http/Controllers/DevicesController.php
+++ spint4_equip5_projecteglobal/app/Http/Controllers/DevicesController.php
@@ -44,6 +44,8 @@ class DevicesController extends Controller
     }
 
     public function eliminar(){
-
+        $dispositiu = new Device();
+        $dateNow = date('Y-m-d');
+        $dispositiu->hidden = $dateNow;
     }
 }
--- spint4_equip5_projecteglobal/app/Http/Controllers/InventoryController.php
+++ spint4_equip5_projecteglobal/app/Http/Controllers/InventoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Device;
 
+
 class InventoryController extends Controller
 {
         public function inventario(){
