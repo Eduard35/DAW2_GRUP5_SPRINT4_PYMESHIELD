783c0cd0125adcec41c831287c2220f81b43b59c
janalmudeve <joanpasqualalmudeve@iesmontsia.org>
2023-02-15 19:15:50 +0100

    Modal Funcionals editar i crear

--- spint4_equip5_projecteglobal/routes/web.php
+++ spint4_equip5_projecteglobal/routes/web.php
@@ -33,7 +33,7 @@ Route::patch('/restaurar', [RestoreController::class, 'restaurar']) ->name('rest
 //Mostrar Dispositivos
 Route::get('/dispositivo', [DevicesController::class, 'mostrarDispositivos']);
 Route::post('/dispositivo', [DevicesController::class, 'crear'])->name('dispositiu.crear');
-Route::put('/dispositivo', [DevicesController::class, 'modificar'])->name('dispositiu.update');
+Route::put('/dispositivo/{id}', [DevicesController::class, 'modificar'])->name('dispositiu.update');
 Route::patch('/dispositivo', [DevicesController::class, 'eliminar'])->name('dispositiu.eliminar');
 
 //Mostrar inventari
