6172b1df34b40aa90881dba60e2507522c9d3255
Eduard35 <eduardandreizahorneanu@iesmontsia.org>
2023-02-20 15:42:58 +0100

    restaurar

--- spint4_equip5_projecteglobal/resources/js/components/restaurar.vue
+++ spint4_equip5_projecteglobal/resources/js/components/restaurar.vue
@@ -31,6 +31,7 @@
                 <td>{{ dispositivo.serial_number }}</td>
                 <td>{{ dispositivo.user_id }}</td>
                 <td>
+                    <form action="{{ route('restaurar.php', $device->id) }}"></form>
                     <button
                         class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                         Restaurar
